4113f1df2574930f5e88632743f0a561c77d4354
serenity39 <69771022+serenity39@users.noreply.github.com>
2025-03-16 22:43:34 +0100

    feat(todo): render todo items as a list below input

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -19,8 +19,26 @@ export default function TodoList() {
 
     return (
         <div>
-            <input type="text" value={newTodo} onChange={handleInputChange} />
-            <button onClick={handleAddClick}>Add Todo</button>
+            <h2>Todo List</h2>
+            <div className="todo-input-wrapper">
+                <input
+                    className="todo-input" 
+                    type="text" 
+                    value={newTodo}
+                    onChange={handleInputChange}
+                    placeholder="Enter a todo item"
+                />
+                <button className="add-button" onClick={handleAddClick}>Add Todo</button>
+            </div>
+            <div className="todo-section">
+                {todos.length === 0 ? (<p className="empty-message">No todos yet!</p>) : (
+                    <ul className="todo-list">
+                        {todos.map(function (todo, index) {
+                            return <li className="todo-item" key={index}>{todo}</li>;
+                            })}
+                        </ul>
+                    )}
+            </div>
         </div>
     );
 }
