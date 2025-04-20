db85a9b5101c02c0d749557771636b7e0ce641f1
serenity39 <69771022+serenity39@users.noreply.github.com>
2025-04-20 16:55:51 +0200

    fixed the navbar styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import  {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
 import Home from './pages/Home';
 import WeatherPage from './pages/WeatherPage';
 import TodoPage from './pages/TodoPage';
@@ -8,19 +8,27 @@ import TodoPage from './pages/TodoPage';
 export default function App() {
     return (
         <Router>
-            <nav className="nav-bar">
-                <ul className="nav-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/todo">To-Do</Link></li>
-                <li><Link to="/weather">Weather</Link></li>
-                </ul>
-            </nav>
+            <div className="app-container">
+                <nav className="nav-bar">
+                    <div className="nav-content">
+                        <NavLink to="/" className="logo-link">
+                            <div className="logo">Dashboard</div>
+                        </NavLink>
+                        <ul className="nav-links">
+                            <li><NavLink to="/todo">To-Do</NavLink></li>
+                            <li><NavLink to="/weather">Weather</NavLink></li>
+                        </ul>
+                    </div>
+                </nav>
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/todo" element={<TodoPage />} />
-                <Route path="/weather" element={<WeatherPage />} />
-            </Routes>
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/todo" element={<TodoPage />} />
+                        <Route path="/weather" element={<WeatherPage />} />
+                    </Routes>
+                </main>
+            </div>
         </Router>
     );
 }
